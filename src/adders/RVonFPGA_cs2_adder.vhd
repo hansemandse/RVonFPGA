146967319e66ae956032320bd4357d6fe3d88d6c
hansemandse <hansjakobdamsgaard@gmail.com>
2019-02-17 16:40:02 +0100

    Update carry-select2

--- src/adders/RVonFPGA_cs2_adder.vhd
+++ src/adders/RVonFPGA_cs2_adder.vhd
@@ -10,17 +10,17 @@ entity cs2_adder is
 end cs2_adder;
 
 architecture rtl of cs2_adder is
-    signal result_low : std_logic_vector(32 downto 0);
-    signal result_high : std_logic_vector(31 downto 0);
+    signal result_low, result_high0, result_high1 : std_logic_vector(32 downto 0);
 begin
     result_low <= std_logic_vector(unsigned('0' & data1(31 downto 0)) + unsigned('0' & data2(31 downto 0)));
+    result_high0 <= std_logic_vector(unsigned(data1(63 downto 32) & '0') + unsigned(data2(63 downto 32) & '1'));
+    result_high1 <= std_logic_vector(unsigned(data1(63 downto 32) & '1') + unsigned(data2(63 downto 32) & '1'));
     process (all)
     begin
         if (result_low(32) = '1') then
-            result_high <= std_logic_vector(unsigned(data1(63 downto 32)) + unsigned(data2(63 downto 32)) + 1);
+            result <= (31 downto 0 => result_low(31 downto 0), 63 downto 32 => result_high1(32 downto 1));
         else
-            result_high <= std_logic_vector(unsigned(data1(63 downto 32)) + unsigned(data2(63 downto 32)));
+            result <= (31 downto 0 => result_low(31 downto 0), 63 downto 32 => result_high0(32 downto 1));
         end if;
     end process;
-    result <= (31 downto 0 => result_low(31 downto 0), 63 downto 32 => result_high);
 end rtl;
