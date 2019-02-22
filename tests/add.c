a5f3f46387630d7e37bb8a103849dc7098e0186d
hansemandse <hansjakobdamsgaard@gmail.com>
2019-02-22 14:05:36 +0100

    Update and add test programs
    Makefile has been made much simpler

--- tests/add.c
+++ tests/add.c
@@ -1,3 +1,7 @@
+// Minimal start up code taken from the 02155 Computer Architecture Engineering lab 7
+asm("li sp, 0x800"); // SP set to 2048
+asm("j main"); // Jump to the main function of the program
+
 int main() {
     long a = 115, b = 7142;
     long c = a + b;
--- tests/sra.c
+++ tests/sra.c
@@ -3,7 +3,7 @@ asm("li sp, 0x800"); // SP set to 2048
 asm("j main"); // Jump to the main function of the program
 
 int main() {
-    long a = 12131412, b = 971246;
-    long c = a - b;
+    long a = 12841298, b = 11;
+    long c = a >> b;
     return 0;
 }
