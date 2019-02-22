51293d840702e7a89e0a71b3ba6dd20afa50e881
hansemandse <hansjakobdamsgaard@gmail.com>
2019-02-22 12:42:00 +0100

    Test programs

--- tests/add.c
+++ tests/add.c
@@ -1,9 +1,5 @@
-long add(long a, long b) {
-    return a + b;
-}
-
 int main() {
     long a = 115, b = 7142;
-    long c = add(a, b);
+    long c = a + b;
     return 0;
 }
---
+++ tests/sub.c
@@ -0,0 +1,5 @@
+int main() {
+    long a = 12131412, b = 971246;
+    long c = a - b;
+    return 0;
+}
